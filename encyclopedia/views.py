679060cbc6f5dc22c80114eaaedbb91a358cef6e
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-07-18 21:32:20 +0300

    put the verify in a different fuction

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import markdown2 as markdown
 from . import util
 
@@ -9,10 +10,9 @@ def index(request):
     Returns:
         Renders the index page.
     '''
-
+    # for entry in util.get_entry():
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
-        "name": index
+        "entries": util.list_entries()
     })
 
 
@@ -33,6 +33,23 @@ def md_to_html(file_name):
     return markdowner.convert(f)
 
 
+def verify(title):
+    '''
+    '''
+    entries = util.list_entries()
+    # Checking if the title is valid - caseinsensitive
+    if title.casefold() not in (entry.casefold() for entry in entries):
+        # If page is not valid, render the 404 page.
+        return HttpResponse(f"Error 404, Page not found\n{title} page you are looking for cannot be found.")
+    else:
+        # If valid, attach the entry name to be used in the rendering of the url
+        for entry in entries:
+            if entry.casefold() == title:
+                title_name = entry
+
+    return title_name
+
+
 def view_page(request, title):
     '''
     Function that renders the page based on the title entered on the url.
@@ -43,18 +60,15 @@ def view_page(request, title):
     Returns:
         page_not_found render if the page is not there OR renders the page with the given title
     '''
-
     file_name = ""
-    
+    entries = util.list_entries()
     # Checking if the title is valid - caseinsensitive
-    if title.casefold() not in (entry.casefold() for entry in util.list_entries()):
+    if title.casefold() not in (entry.casefold() for entry in entries):
         # If page is not valid, render the 404 page.
-        return render(request, "encyclopedia/page_not_found.html", {
-            "name": title,
-        })
+        return render(request, "encyclopedia/page_not_found.html")
     else:
         # If valid, attach the entry name to be used in the rendering of the url
-        for entry in util.list_entries():
+        for entry in entries:
             if entry.casefold() == title:
                 file_name = entry
 
@@ -62,9 +76,5 @@ def view_page(request, title):
     content = md_to_html(file_name)
     return render(request, "encyclopedia/view_page.html", {
         "content": content,
-        "name": title,
+        "name": file_name,
     })
-
-
-def page_redirect(request):
-    pass
