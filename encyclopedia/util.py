9d169e3f9fa6d05e01f5c797d3c940613326fc45
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-07-26 04:10:25 +0300

    Add url paths to the list in the index path

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -4,6 +4,8 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.http import HttpResponse
 
+import markdown2 as markdown
+
 def list_entries():
     """
     Returns a list of all names of encyclopedia entries.
@@ -36,3 +38,39 @@ def get_entry(title):
     except FileNotFoundError:
         return None
 
+def md_to_html(file_name):
+    '''
+    Function that converts markdown to HTML to be rendered to the template.
+
+    Args:
+        file_name: (str) the name of the file to be converted
+
+    Returns: 
+        The converted code in HTML.
+    '''
+
+    markdowner = markdown.Markdown()
+    # get the markdown file
+    try:
+        f = get_entry(file_name)
+    except:
+        f = f'{file_name} not found!'
+    return markdowner.convert(f)
+
+
+def search_for_file(request, title):
+    file_name = ""
+    entries = list_entries()
+    if title.casefold() not in (entry.casefold() for entry in entries):
+        # If page is not valid, render the 404 page.
+        context = {
+            "name": title,
+        }
+        return render(request, "encyclopedia/page_not_found.html", context)
+    else:
+        # If valid, attach the entry name to be used in the rendering of the url
+        for entry in entries:
+            if entry.casefold() == title:
+                file_name = entry
+    
+    return file_name
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import markdown2 as markdown
 from . import util
+
 
 def index(request):
     '''
@@ -16,41 +15,6 @@ def index(request):
     })
 
 
-def md_to_html(file_name):
-    '''
-    Function that converts markdown to HTML to be rendered to the template.
-
-    Args:
-        file_name: (str) the name of the file to be converted
-
-    Returns: 
-        The converted code in HTML.
-    '''
-
-    markdowner = markdown.Markdown()
-    # get the markdown file
-    f = util.get_entry(file_name)
-    return markdowner.convert(f) 
-
-
-# def verify(title):
-#     '''
-#     '''
-#     title_name = ""
-#     entries = util.list_entries()
-#     # Checking if the title is valid - caseinsensitive
-#     if title.casefold() not in (entry.casefold() for entry in entries):
-#         # If page is not valid, render the 404 page.
-#         return HttpResponse(f"Error 404, Page not found\n{title} page you are looking for cannot be found.")
-#     else:
-#         # If valid, attach the entry name to be used in the rendering of the url
-#         for entry in entries:
-#             if entry.casefold() == title:
-#                 title_name = entry
-
-#     return str(title_name)
-
-
 def view_page(request, title):
     '''
     Function that renders the page based on the title entered on the url.
@@ -61,22 +25,14 @@ def view_page(request, title):
     Returns:
         page_not_found render if the page is not there OR renders the page with the given title
     '''
-    file_name = ""
-    entries = util.list_entries()
-    # Checking if the title is valid - caseinsensitive
-    if title.casefold() not in (entry.casefold() for entry in entries):
-        # If page is not valid, render the 404 page.
-        return render(request, "encyclopedia/page_not_found.html", {
-            "name":title
-        })
-    else:
-        # If valid, attach the entry name to be used in the rendering of the url
-        for entry in entries:
-            if entry.casefold() == title:
-                file_name = entry
+    # entries = util.list_entries()
 
     # Converting the markdown to html to render to page
-    content = md_to_html(file_name) 
+
+    file_name = util.search_for_file(request, title)
+
+    content = util.md_to_html(file_name) 
+
     return render(request, "encyclopedia/view_page.html", {
         "content": content,
         "name": file_name,
